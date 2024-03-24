e7b72b1329b54916841c7b9c17bd502830c3a770
Adam Plesnik <adam.plesnik@expersoft.com>
2024-03-24 17:19:03 +0100

    Add 'Inter' font family
    * add woff2
    * set Inter + default sans as theme font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
