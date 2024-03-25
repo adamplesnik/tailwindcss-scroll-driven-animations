f44bad98d7eef7a2d43e53235172f8a445045115
Adam Plesnik <adam.plesnik@expersoft.com>
2024-03-25 14:09:33 +0100

    Add page bg gradient, base layout with responsivity, base bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import PageBackground from './components/PageBackground.tsx'
 
+function App() {
   return (
-    <>
-      Silence.
-    </>
+    <div className={'flex justify-center p-8 md:p-16 lg:p-20'}>
+      <PageBackground />
+      <div className={'flex w-full max-w-screen-md flex-col gap-4 md:gap-8'}></div>
+    </div>
   )
 }
 
---
+++ src/components/PageBackground.tsx
@@ -0,0 +1,11 @@
+const PageBackground = () => {
+  return (
+    <div
+      className={
+        'absolute left-0 top-0 -z-10 h-[12rem] w-full bg-gradient-to-tr from-yellow-300/0 from-20% via-yellow-300/30 via-75% to-purple-700/80 to-[130%] md:h-[24rem] md:from-55% lg:h-[42rem]'
+      }
+    ></div>
+  )
+}
+
+export default PageBackground
