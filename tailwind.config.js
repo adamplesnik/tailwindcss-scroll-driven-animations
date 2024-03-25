e8b3fd14b40b96ccce55ae1846bc7d09d1af6a86
Adam Plesnik <adam.plesnik@expersoft.com>
2024-03-25 13:28:59 +0100

    First version of scrollable animations plugin
    Define base utility classes:
    - animation-timeline
    - scroll-timeline
    - view-timeline
    - timeline-scope

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
 const defaultTheme = require('tailwindcss/defaultTheme')
 
-export default {
+module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
     extend: {
@@ -10,5 +11,94 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(
+      function ({ matchUtilities, theme }) {
+        matchUtilities(
+          {
+            'animation-timeline': (value, { modifier }) => {
+              if (modifier) {
+                return {
+                  'animation-timeline': `--${modifier}`,
+                }
+              } else {
+                return {
+                  animationTimeline: value,
+                }
+              }
+            },
+          },
+          {
+            values: {
+              DEFAULT: 'scroll(y)',
+              auto: 'auto',
+              none: 'none',
+              'scroll-x': 'scroll(x)',
+              view: 'view()',
+            },
+            modifiers: 'any',
+          }
+        )
+
+        matchUtilities(
+          {
+            'scroll-timeline': (value, { modifier }) => {
+              if (modifier) {
+                return {
+                  scrollTimeline: `--${modifier} ${value}`,
+                }
+              } else {
+                return {
+                  scrollTimeline: `none ${value}`,
+                }
+              }
+            },
+          },
+          {
+            values: theme('timelineValues'),
+            modifiers: 'any',
+          }
+        )
+
+        matchUtilities(
+          {
+            'view-timeline': (value, { modifier }) => {
+              if (modifier) {
+                return {
+                  viewTimeline: `--${modifier} ${value}`,
+                }
+              } else {
+                return {
+                  viewTimeline: `none ${value}`,
+                }
+              }
+            },
+          },
+          {
+            values: theme('timelineValues'),
+            modifiers: 'any',
+          }
+        )
+
+        matchUtilities(
+          {
+            'timeline-scope': (value, { modifier }) => ({
+              timelineScope: `--${modifier}`,
+            }),
+          },
+          { values: { DEFAULT: '' }, modifiers: 'any' }
+        )
+      },
+
+      {
+        theme: {
+          timelineValues: {
+            DEFAULT: 'y',
+            block: 'block',
+            x: 'x',
+          },
+        },
+      }
+    ),
+  ],
 }
