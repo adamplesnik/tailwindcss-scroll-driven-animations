f5407ef4ce566d4e739cada3589b6393667ab60a
Adam Plesnik <adam.plesnik@expersoft.com>
2024-03-28 12:00:14 +0100

    Update dark mode switch, add dark class directly to <body>

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,13 @@ function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
   const [mode, setMode] = useLocalStorage('mode', defaultDark ? 'dark' : 'light')
   const switchMode = () => {
-    setMode(mode === 'light' ? 'dark' : 'light')
+    const isLight = mode === 'light'
+    setMode(isLight ? 'dark' : 'light')
+    const classList = document.body.classList
+    isLight ? classList.add('dark') : classList.remove('dark')
   }
   return (
-    <div
-      className={
-        'flex justify-center bg-zinc-50 p-8 text-zinc-950 md:p-16 lg:p-20 dark:bg-gray-900 dark:text-zinc-300 ' +
-        'scope/content ' +
-        mode
-      }
-    >
+    <div className={'scope/content flex justify-center'}>
       <PageBackground />
       <div className={'relative z-10 w-full max-w-screen-md'}>
         <Header mode={mode} onClick={switchMode} />
