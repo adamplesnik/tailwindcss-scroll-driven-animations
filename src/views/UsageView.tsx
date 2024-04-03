69a4f2000d8a54cc49a0aa076850291d0f5b7e61
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-03 14:09:08 +0200

    Tune docu in progress message

--- src/views/UsageView.tsx
+++ src/views/UsageView.tsx
@@ -8,6 +8,10 @@ const UsageView = () => {
   return (
     <Page>
       <Heading size={1}>How to Use This Plugin</Heading>
+      <div className="mb-4 mt-8 flex items-center gap-4 rounded-lg border border-sky-300 bg-sky-100 p-2 text-sm  dark:border-sky-600 dark:bg-sky-700/30">
+        <Info className="size-6 shrink-0 text-sky-500 dark:text-sky-600" strokeWidth="1.5" />
+        Documentation is in progress.
+      </div>
       <div className="mb-4 hidden gap-4 rounded-lg border border-amber-400 bg-amber-50 p-4 text-sm supports-no-animations:flex dark:border-amber-600 dark:bg-stone-700/30">
         <WandSparkles
           className="size-6 shrink-0 text-amber-500 dark:text-amber-600"
@@ -17,10 +21,7 @@ const UsageView = () => {
         see the demos working correctly.
       </div>
       <Usage />
-      <div className="mb-4 mt-8 flex items-center gap-2 rounded-lg border border-sky-300 bg-sky-100 p-2 text-sm  dark:border-sky-600 dark:bg-sky-700/30">
-        <Info className="size-5 shrink-0 text-sky-500 dark:text-sky-600" strokeWidth="1.5" />
-        Documentation is in progress.
-      </div>
+
       <Demo />
     </Page>
   )
