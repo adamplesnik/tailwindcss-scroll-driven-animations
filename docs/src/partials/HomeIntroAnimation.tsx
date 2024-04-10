e272af6fc5fd84a2bb2458377dff405edd6d3b33
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-10 12:37:10 +0200

    Add code block, prismjs, home anim WIP

--- docs/src/partials/HomeIntroAnimation.tsx
+++ docs/src/partials/HomeIntroAnimation.tsx
@@ -1,19 +1,47 @@
+import CodeBlock from '../components/CodeBlock'
+
+const introAnimCSS = `@keyframes appear {
+  to {
+    width: auto;
+  }
+}
+
+.animate-appear {
+  animation: appear 0s linear forwards;
+}`
+
+const introAnimTSX = `{text.map((letter, index) => (
+  <span
+    className="inline-block w-0 animate-appear overflow-hidden timeline"
+    style={{
+      animationRangeEnd: (index * 64 + 128).toString() + 'px',
+    }}
+    key={index}
+  >
+    {letter === ' ' ? '\\u00A0' : letter}
+  </span>
+))}`
+
 const HomeIntroAnimation = () => {
   const text = Array.from('Scroll. Your next superpower.')
   return (
     <div className="h-[1920px] w-full">
-      <div className="flex w-full font-bold text-3xl sm:text-7xl sticky top-32">
-        {text.map((letter, index) => (
-          <span
-            className="inline-block w-0 animate-appear overflow-hidden timeline"
-            style={{
-              animationRangeEnd: (index * 64 + 128).toString() + 'px',
-            }}
-            key={index}
-          >
-            {letter === ' ' ? '\u00A0' : letter}
-          </span>
-        ))}
+      <div className="sticky top-32">
+        <div className="flex w-full font-bold text-3xl sm:text-7xl">
+          {text.map((letter, index) => (
+            <span
+              className="inline-block w-0 animate-appear overflow-hidden timeline"
+              style={{
+                animationRangeEnd: (index * 64 + 128).toString() + 'px',
+              }}
+              key={index}
+            >
+              {letter === ' ' ? '\u00A0' : letter}
+            </span>
+          ))}
+        </div>
+        <CodeBlock language="css">{introAnimCSS}</CodeBlock>
+        <CodeBlock language="tsx">{introAnimTSX}</CodeBlock>
       </div>
     </div>
   )
