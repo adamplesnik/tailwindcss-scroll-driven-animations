a61abb95871d5b92b2fd7f6dcb4541f631f38e30
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-11 23:35:43 +0200

    Update demo wrapper, add action button

--- src/demos/DemoWrapper.tsx
+++ src/demos/DemoWrapper.tsx
@@ -1,15 +1,42 @@
 import { PropsWithChildren } from 'react'
 import { addWithSpace } from '../utils/addWithSpace'
+import ActionButton from '../components/ActionButton'
+import { Repeat, StepForward } from 'lucide-react'
 
-const DemoWrapper = ({ children, className = '' }: PropsWithChildren<DemoWrapperProps>) => {
+const replayButtonClick = (element: string) => {
+  const wrapper = document.getElementById(element)
+  wrapper &&
+    wrapper.getAnimations({ subtree: true }).forEach((anim) => {
+      anim.cancel()
+      anim.play()
+    })
+}
+
+const DemoWrapper = ({
+  children,
+  className = '',
+  actionButton = false,
+  actionButtonElement = 'element',
+}: PropsWithChildren<DemoWrapperProps>) => {
   return (
     <div
+      id={actionButtonElement}
       className={
         'relative mb-4 h-96 overflow-y-auto overflow-x-hidden rounded-lg border border-zinc-300 bg-zinc-100/70 dark:border-zinc-700 dark:bg-black/10' +
         addWithSpace(className)
       }
     >
       {children}
+      {actionButton && (
+        <div className="absolute right-3 top-3">
+          <ActionButton
+            Icon={Repeat}
+            IconOnClick={StepForward}
+            clickAction={() => replayButtonClick(actionButtonElement)}
+            tooltip="Replay"
+          />
+        </div>
+      )}
     </div>
   )
 }
@@ -17,6 +44,8 @@ const DemoWrapper = ({ children, className = '' }: PropsWithChildren<DemoWrapper
 export interface DemoWrapperProps {
   children: PropsWithChildren
   className?: string
+  actionButton?: boolean
+  actionButtonElement?: string
 }
 
 export default DemoWrapper
