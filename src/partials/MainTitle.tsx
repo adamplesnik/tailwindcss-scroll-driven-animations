9c57b0ae59524baf3fc3a445e85d21eee3d7eb52
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-16 09:54:01 +0200

    Remove Tailwind job application info from the title

--- src/partials/MainTitle.tsx
+++ src/partials/MainTitle.tsx
@@ -5,19 +5,7 @@ import Paragraph from '../components/Paragraph.tsx'
 const MainTitle = () => {
   return (
     <>
-      <Heading size={1}>
-        Hello, I am Adam Plesník. This website serves as my application for the Tailwind Labs Design
-        Engineer position. It features the Scroll-driven Animations{' '}
-        <Link
-          href="https://github.com/adamplesnik/tailwindcss-scroll-driven-animations"
-          target="_blank"
-          borderWidth="huge"
-          inline
-        >
-          plugin
-        </Link>{' '}
-        I&nbsp;created.
-      </Heading>
+      <Heading size={1}>Scroll-driven Animations for Tailwind CSS</Heading>
       <Paragraph size={'small'} className={'pb-12 pt-6 text-zinc-600 dark:text-zinc-400'}>
         Bratislava, Slovakia,{' '}
         <Link href="https://adamplesnik.com" target="_blank" borderWidth="narrow">
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,13 +1,11 @@
 import Page from '../layouts/Page.tsx'
 import Animations from '../partials/Animations.tsx'
-import Intro from '../partials/Intro.tsx'
 import MainTitle from '../partials/MainTitle.tsx'
 
 function HomeView() {
   return (
     <Page>
       <MainTitle />
-      <Intro />
       <Animations />
     </Page>
   )
