44545f6d081fc17edda88946405cee50a14cbf94
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-16 11:33:38 +0200

    Remove my name from the title

--- docs/src/partials/MainTitle.tsx
+++ docs/src/partials/MainTitle.tsx
@@ -1,19 +1,7 @@
 import Heading from '../components/Heading.tsx'
-import Link from '../components/Link.tsx'
-import Paragraph from '../components/Paragraph.tsx'
 
 const MainTitle = () => {
-  return (
-    <>
-      <Heading size={1}>Scroll-driven Animations for Tailwind CSS</Heading>
-      <Paragraph size={'small'} className={'pb-12 pt-6 text-zinc-600 dark:text-zinc-400'}>
-        Created by Adam Plesník, Bratislava, Slovakia,{' '}
-        <Link href="https://adamplesnik.com" target="_blank" borderWidth="narrow">
-          adamplesnik.com
-        </Link>
-      </Paragraph>
-    </>
-  )
+  return <Heading size={1}>Scroll-driven Animations for Tailwind CSS</Heading>
 }
 
 export default MainTitle
