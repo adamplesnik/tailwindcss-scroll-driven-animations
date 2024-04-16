5c1c63b6447e6b29acfffc0aed08d124fd3577c2
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-16 11:17:56 +0200

    Remove 'me' from nav, delete .htaccess

--- docs/src/partials/Nav.tsx
+++ docs/src/partials/Nav.tsx
@@ -1,16 +1,13 @@
-import { ArrowUpRight, Github } from 'lucide-react'
+import { Github } from 'lucide-react'
 import DarkModeSwitch from '../components/DarkModeSwitch.tsx'
 import HeaderNavAnchor from '../components/HeaderNavAnchor.tsx'
 
 const Nav = () => {
   return (
-    <div className="no-animations:translate-y-0 sticky top-0 z-20 flex w-full -translate-y-24 animate-translate-down items-stretch justify-center border-b border-b-slate-200/50 bg-white/20 text-zinc-800 shadow-lg backdrop-blur-sm timeline range/0%,200px dark:border-b-slate-600/50 dark:bg-slate-800/60 dark:text-zinc-300">
+    <div className="sticky top-0 z-20 flex w-full -translate-y-24 animate-translate-down items-stretch justify-center border-b border-b-slate-200/50 bg-white/20 text-zinc-800 shadow-lg backdrop-blur-sm timeline range/0%,200px no-animations:translate-y-0 dark:border-b-slate-600/50 dark:bg-slate-800/60 dark:text-zinc-300">
       <div className="flex w-full max-w-screen-lg items-stretch gap-2 px-8 py-2 md:px-16 lg:px-20">
         <HeaderNavAnchor to="/#plugin">Plugin</HeaderNavAnchor>
         <HeaderNavAnchor to="/docs#documentation">Docs</HeaderNavAnchor>
-        <HeaderNavAnchor to="https://adamplesnik.com" external>
-          Me <ArrowUpRight className="ml-2 size-4" />
-        </HeaderNavAnchor>
         <div className="flex-1" />
         <DarkModeSwitch />
         <a
