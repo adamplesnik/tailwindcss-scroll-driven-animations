33428d817f39190924bab91a18a5c22be5ebe1ec
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-16 11:22:20 +0200

    Update docs

--- docs/src/views/DocsView.tsx
+++ docs/src/views/DocsView.tsx
@@ -1,6 +1,4 @@
 import { WandSparkles } from 'lucide-react'
-import Heading from '../components/Heading'
-import Paragraph from '../components/Paragraph'
 import Docs from '../docs/Docs'
 import Page from '../layouts/Page'
 import Demo from '../partials/Demo'
@@ -8,11 +6,7 @@ import Demo from '../partials/Demo'
 const DocsView = () => {
   return (
     <Page>
-      <Heading size={1}>Scroll-driven Animations</Heading>
-      <Paragraph size="small" className="opacity-50">
-        Tailwind CSS Plugin
-      </Paragraph>
-      <div className="no-animations:flex mt-12 hidden gap-4 rounded-lg border border-amber-400 bg-amber-50 p-4 text-sm dark:border-amber-600 dark:bg-stone-700/30">
+      <div className="hidden gap-4 rounded-lg border border-amber-400 bg-amber-50 p-4 text-sm no-animations:flex dark:border-amber-600 dark:bg-stone-700/30">
         <WandSparkles
           className="size-6 shrink-0 text-amber-500 dark:text-amber-600"
           strokeWidth="1"
