adeee8ded7fc9c482f84366b8432883ba2710829
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-17 15:53:13 +0200

    Update code, codeblock styling

--- docs/src/components/Code.tsx
+++ docs/src/components/Code.tsx
@@ -1,31 +1,31 @@
-import Prism from "prismjs";
-import { PropsWithChildren, useEffect } from "react";
-import { addWithSpace } from "../utils/addWithSpace";
+import Prism from 'prismjs'
+import { PropsWithChildren, useEffect } from 'react'
+import { addWithSpace } from '../utils/addWithSpace'
 
 const Code = ({
   children,
-  language = "css",
-  className = "",
+  language = 'css',
+  className = '',
 }: PropsWithChildren<InlineCodeProps>) => {
   useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+    Prism.highlightAll()
+  }, [])
   return (
     <code
       className={
-        `language-${language} rounded bg-zinc-300/60 px-1 py-0.5 font-mono text-sm text-zinc-900/60 dark:bg-indigo-200/20 dark:text-zinc-300` +
+        `language-${language} whitespace-nowrap rounded bg-fuchsia-200/40 p-1 px-1 py-0.5 font-mono text-sm text-zinc-900 dark:bg-indigo-200/20 dark:text-zinc-300` +
         addWithSpace(className)
       }
     >
       {children}
     </code>
-  );
-};
-
-export interface InlineCodeProps {
-  children: PropsWithChildren;
-  language?: "javascript" | "css" | "html";
-  className?: string;
+  )
 }
 
-export default Code;
+export interface InlineCodeProps {
+  children: PropsWithChildren
+  language?: 'javascript' | 'css' | 'html'
+  className?: string
+}
+
+export default Code
