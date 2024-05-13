d6cf13a17fe757d1ca0561cf0a92115b1421cbb7
Adam Plesnik <adam.plesnik@expersoft.com>
2024-05-13 16:32:21 +0200

    Update animations, plugins

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+import plugin from 'tailwindcss/plugin'
 
 module.exports = {
   darkMode: 'selector',
@@ -17,6 +18,7 @@ module.exports = {
         appear: 'appear auto cubic-bezier(0.65, 0.05, 0.17, 0.99) forwards',
         opacity: 'opacity 2s cubic-bezier(0.65, 0.05, 0.17, 0.99) forwards',
         'scale-to-right': 'scale-to-right auto linear forwards',
+        'to-stroke-dashoffset-0': 'to-stroke-dashoffset-0 5s ease-in-out forwards',
         'translate-down': 'translate-down 3s cubic-bezier(0.65, 0.05, 0.17, 0.99) forwards',
         'translate-up': 'translate-up auto ease-in-out forwards',
       },
@@ -24,7 +26,33 @@ module.exports = {
         bounce: 'cubic-bezier(0.26, 0.53, 1, 0.63)',
         line: 'cubic-bezier(0.65, 0.05, 0.17, 0.99)',
       },
+      keyframes: {
+        'to-stroke-dashoffset-0': {
+          to: { strokeDashoffset: 0 },
+        },
+      },
     },
   },
-  plugins: [require('@adam.plesnik/tailwindcss-scroll-driven-animations')],
+  plugins: [
+    require('@adam.plesnik/tailwindcss-scroll-driven-animations'),
+    plugin(function ({ matchUtilities, addVariant }) {
+      matchUtilities(
+        {
+          'dash-offset': (value, { modifier }) => ({
+            strokeDashoffset: modifier,
+          }),
+        },
+        { values: { DEFAULT: '' }, modifiers: 'any' }
+      )
+      matchUtilities(
+        {
+          'dash-array': (value, { modifier }) => ({
+            strokeDasharray: modifier,
+          }),
+        },
+        { values: { DEFAULT: '' }, modifiers: 'any' }
+      )
+      addVariant('path', '& > path')
+    }),
+  ],
 }
